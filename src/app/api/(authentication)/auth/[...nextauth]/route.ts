816df13acec9090d5124d33c097102f58a3a9dc9
suyog prasai <suyog224356@gmail.com>
2026-06-27 12:30:45 +0000

    fresh

--- src/app/api/(authentication)/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-// app/api/auth/[...nextauth]/route.js
-import NextAuth from "next-auth";
-import { authOptions } from "./options";
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
--- src/helpers/extract-name-and-email.ts
+++
@@ -1,4 +0,0 @@
-export function extractRollAndName(email: string): { roll: string; name: string } | null {
-    const match = email.match(/^(\d+)([a-zA-Z]+)@bnks\.edu\.np$/);
-    return match ? { roll: match[1], name: match[2] } : null;
-}
